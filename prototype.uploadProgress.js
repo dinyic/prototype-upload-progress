e86289990b8a0e19cddf68f3f5d4a48857e26dce
Nick Plante <nap@zerosum.org>
2009-03-02 18:14:28 -0500

    remove broken safari iframe stuff, add dummy animation

--- prototype.uploadProgress.js
+++ prototype.uploadProgress.js
@@ -2,6 +2,7 @@
  * prototype.uploadProgress
  *
  * Copyright (c) 2008 Peter Sarnacki (drogomir.com)
+ * Modified by Nick Plante, 2009 (blog.zerosum.org)
  *
  * Licensed under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
@@ -25,61 +26,40 @@ var UploadProgressMethods = {
       complete: function() {},
       success: function() {},
       error: function() {},
-      uploadProgressPath: '/javascripts/prototype.js',
-      prototypePath: '/javascripts/admin/uploadProgress.js',
       server_timer: "",
-      client_timer: ""
+      client_timer: "",
     }, options);
     
-    /* tried to add iframe after submit (to not always load it) but it won't work. 
-    safari can't get scripts properly while submitting files */
-    if (Prototype.Browser.WebKit && top.document == document) {
-    
-      /* iframe to send ajax requests in safari 
-      thanks to Michele Finotto for idea */
-      iframe = document.createElement('iframe');
-      iframe.name = "progressFrame";
-      $(iframe).setStyle({width: '0', height: '0', position: 'absolute', top: '-3000px'});
-      document.body.appendChild(iframe);
-      
-      var d = iframe.contentWindow.document;
-      d.open();
-      /* weird - safari won't load scripts without this lines... */
-      d.write('<html><head></head><body></body></html>');
-      d.close();
-      
-      var b = d.body;
-      var s = d.createElement('script');
-      s.src = options.prototypePath;
-      /* must be sure that prototype is loaded */
-      s.onload = function() {
-        var s1 = d.createElement('script');
-        s1.src = options.uploadProgressPath;
-        b.appendChild(s1);
-      }
-      b.appendChild(s);
-    }
-  
-    Event.observe(element, 'submit', function() {
-      var uuid = "";
-      for (i = 0; i < 32; i++) { uuid += Math.floor(Math.random() * 16).toString(16); }
-      
-      options.uuid = uuid;
-      /* start callback */
-      options.start();
-      
-      /* patch the form-action tag to include the progress-id 
-         if X-Progress-ID has been already added just replace it */
-      if (old_id = /X-Progress-ID=([^&]+)/.exec($(this).readAttribute("action"))) {
-        var action = $(this).readAttribute("action").replace(old_id[1], uuid);
-        $(this).writeAttribute("action", action);
+    Event.observe(element, 'submit', function(ev) {
+      /* safari can't get scripts properly while submitting files because it's braindead */
+      if (Prototype.Browser.WebKit) {
+        $(options.progressContainer).show();
+        setInterval(function() { 
+          options.current_percent = options.current_percent + 10;
+          if (options.current_percent > 100) options.current_percent = 0;
+          $(options.progressBar).setStyle({width: options.current_percent + '%'});
+        }, options.client_interval);
       } else {
-        $(this).writeAttribute("action", $(this).readAttribute("action") + "?X-Progress-ID=" + uuid);
+        var uuid = "";
+        for (i = 0; i < 32; i++) { uuid += Math.floor(Math.random() * 16).toString(16); }
+      
+        options.uuid = uuid;
+        /* start callback */
+        options.start();
+      
+        /* patch the form-action tag to include the progress-id 
+           if X-Progress-ID has been already added just replace it */
+        if (old_id = /X-Progress-ID=([^&]+)/.exec($(this).readAttribute("action"))) {
+          var action = $(this).readAttribute("action").replace(old_id[1], uuid);
+          $(this).writeAttribute("action", action);
+        } else {
+          $(this).writeAttribute("action", $(this).readAttribute("action") + "?X-Progress-ID=" + uuid);
+        }
+        var uploadProgress = Prototype.uploadProgress;
+        var uploadMovement = Prototype.uploadMovement;
+        options.server_timer = window.setInterval(function() { uploadProgress(this, options) }, options.interval);
+        options.client_timer = window.setInterval(function() { uploadMovement(this, options) }, options.client_interval);
       }
-      var uploadProgress = Prototype.Browser.WebKit ? progressFrame.Prototype.uploadProgress : Prototype.uploadProgress;
-      var uploadMovement = Prototype.Browser.WebKit ? progressFrame.Prototype.uploadMovement : Prototype.uploadMovement;
-      options.server_timer = window.setInterval(function() { uploadProgress(this, options) }, options.interval);
-      options.client_timer = window.setInterval(function() { uploadMovement(this, options) }, options.client_interval);
     });
   }
 };
@@ -91,13 +71,11 @@ PrototypeUploadProgressMethods = {
     if (options.current_percent >= 100) {
       window.clearTimeout(options.client_timer);
     } else {
-      var container = Prototype.Browser.WebKit ? parent.document.getElementById(options.progressContainer) : $(options.progressContainer);
-      container.show();
+      $(options.progressContainer).show();
 
       options.current_percent = options.current_percent + options.rate
       if (options.current_percent > 100) options.current_percent = 100;
-      var bar = Prototype.Browser.WebKit ? parent.document.getElementById(options.progressBar) : $(options.progressBar);
-      bar.setStyle({width: Math.floor(options.current_percent) + '%'});
+      $(options.progressBar).setStyle({width: Math.floor(options.current_percent) + '%'});
     }
   },
       
